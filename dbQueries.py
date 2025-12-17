27b2505c831d63e4d79c9cea40aabd4a4728ea3a
Alexandru Comaniciu <comaniciu.nicolae@gmail.com>
2025-12-17 10:39:16 +0200

    Last change before Christmas event

--- dbQueries.py
+++ dbQueries.py
@@ -1,4 +1,5 @@
 import os
+import random
 import mysql.connector  
 from dotenv import load_dotenv
 from urllib.parse import urlparse
@@ -67,6 +68,52 @@ class dbQueries():
                     connection.close() 
                     print("Database connection closed!")
             return
+
+    def insertDummyLine(self):
+        try:
+            connection = mysql.connector.connect(**self.config)
+            if connection.is_connected():
+                print("Successfully connected to the database!")
+                cursor = connection.cursor()
+
+                table_configs = [
+                    {
+                        'table': 'bere',
+                        'columns': ['Heineken', 'Corona', 'Peroni'],
+                        'insert_query': "update bere set Heineken = %s, Corona = %s, Peroni = %s WHERE id = (SELECT max_id FROM (SELECT MAX(id) AS max_id FROM bere) AS tmp)",
+                        'values': (random.randint(1,15),random.randint(1,15),random.randint(1,15)),
+                        'empty': True
+                    },
+                    {
+                        'table': 'vin',
+                        'columns': ['Aperol_Spritz', 'Vin_Rosu', 'Vin_Alb', 'Prosecco'],
+                        'insert_query': "update vin set Aperol_Spritz = %s, Vin_Rosu = %s, Vin_Alb = %s, Prosecco = %s  WHERE id = (SELECT max_id FROM (SELECT MAX(id) AS max_id FROM vin) AS tmp)",
+                        'values': (random.randint(5,20),random.randint(5,20),random.randint(5,20), random.randint(5,20)),
+                        'empty': True
+                    },
+                    {
+                        'table': 'racoritoare',
+                        'columns': ['Vin_Spumant_Fara_Alcool','Apa', 'Cola'],
+                        'insert_query': "update racoritoare set Vin_Spumant_Fara_Alcool = %s, Apa = %s, Cola = %s WHERE id = (SELECT max_id FROM (SELECT MAX(id) AS max_id FROM racoritoare) AS tmp)",
+                        'values': (random.randint(1,10),random.randint(1,10),random.randint(1,10)),
+                        'empty': True
+                    }
+                ]
+
+                for config in table_configs:
+                    cursor.execute(config["insert_query"], config["values"])
+                    connection.commit()
+                    print(f"Successfully inserted new row in {config["table"]} with {', '.join(config["columns"])} = 0.")
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            if 'connection' in locals():
+                connection.rollback()
+        finally:
+            if "connection" in locals() and connection.is_connected():
+                cursor.close()
+                connection.close() 
+                print("Database connection closed!")
+        return
 
     def insertLine(self):
         try:
